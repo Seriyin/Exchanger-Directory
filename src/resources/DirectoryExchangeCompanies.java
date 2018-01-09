999e2f4e6fd7cc24691912f74df41d4f6aa7bd12
Seriyin <redan.blue27@gmail.com>
2018-01-09 21:42:45 +0000

    Added GET of set of companies an exchange trades.

---
+++ src/resources/DirectoryExchangeCompanies.java
@@ -0,0 +1,40 @@
+package resources;
+
+import io.dropwizard.servlets.assets.ResourceNotFoundException;
+import model.Exchanges;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A resource that handles GETting all companies traded in a given exchange.
+ */
+@Path("/exchanges/{/name}/companies")
+@Produces(MediaType.APPLICATION_JSON)
+public class DirectoryExchangeCompanies
+{
+    private Exchanges exchanges;
+
+    public DirectoryExchangeCompanies(Exchanges exchanges)
+    {
+        this.exchanges = exchanges;
+    }
+
+    @GET
+    public Set<String> getCompanies(@PathParam("name") String name) {
+        if(exchanges.getExchanges()
+                    .containsKey(name))
+        {
+            return exchanges.getExchanges()
+                            .get(name)
+                            .getCompanies()
+                            .keySet();
+        }
+        else {
+            throw new ResourceNotFoundException(
+                    new WebApplicationException());
+        }
+    }
+}
