d7b9b9ad42939be1051b1b75603c18063d751785
Seriyin <redan.blue27@gmail.com>
2018-01-09 18:31:06 +0000

    Added a Resource to GET all exchanges listed.

---
+++ src/DirectoryExchanges.java
@@ -0,0 +1,34 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.net.InetSocketAddress;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * The main global listing of exchanges URL resource.
+ */
+@Path("/exchanges")
+@Produces(MediaType.APPLICATION_JSON)
+public class DirectoryExchanges
+{
+    private Exchanges exchanges;
+
+    public DirectoryExchanges(Exchanges exchanges)
+    {
+        this.exchanges = exchanges;
+    }
+
+
+    @GET
+    public Map<String, String> getExchanges() {
+        return exchanges.getExchanges()
+                 .entrySet()
+                 .stream()
+                 .map(e -> new AbstractMap.SimpleImmutableEntry<String,String>(e.getKey(), e.getValue().getName()))
+                 .collect(Collectors.toMap(AbstractMap.SimpleImmutableEntry::getKey,
+                                           AbstractMap.SimpleImmutableEntry::getValue));
+    }
+}
