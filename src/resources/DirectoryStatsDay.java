78615f6f2f4b3dac0b16b0f7dbad0d0bfed90975
Seriyin <redan.blue27@gmail.com>
2018-01-09 21:44:21 +0000

    Added GET of all stats kept for a given company in an exchange for either current or yesterday.

---
+++ src/resources/DirectoryStatsDay.java
@@ -0,0 +1,58 @@
+package resources;
+
+import io.dropwizard.servlets.assets.ResourceNotFoundException;
+import model.Company;
+import model.CompanyDay;
+import model.Exchanges;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Get live-stats for one day (either current or yester).
+ */
+@Path("/exchanges/{/name}{/company}{/day}")
+@Produces(MediaType.APPLICATION_JSON)
+public class DirectoryStatsDay
+{
+    private Exchanges exchanges;
+
+    public DirectoryStatsDay(Exchanges exchanges)
+    {
+        this.exchanges = exchanges;
+    }
+
+    @GET
+    public CompanyDay getStats(@PathParam("name") String name,
+                               @PathParam("company") String company,
+                               @PathParam("day") String day) {
+        if(day.equals("yesterday") || day.equals("today"))
+        {
+            if (exchanges.getExchanges().containsKey(name))
+            {
+                if(exchanges.getExchanges()
+                            .get(name)
+                            .getCompanies()
+                            .containsKey(company))
+                {
+                    CompanyDay result;
+                    if(day.equals("today")) {
+                        result = exchanges.getExchanges()
+                                          .get(name)
+                                          .getCompanies()
+                                          .get(company).createDay(true);
+                    }
+                    else {
+                        result = exchanges.getExchanges()
+                                              .get(name)
+                                              .getCompanies()
+                                              .get(company)
+                                              .createDay(false);
+                    }
+                    return result;
+                }
+            }
+        }
+        throw new ResourceNotFoundException(new WebApplicationException());
+    }
+}
