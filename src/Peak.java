b0fb0ac186fde356692dcd6ba62b27591f3ee84f
Seriyin <redan.blue27@gmail.com>
2018-01-09 18:09:43 +0000

    Simple data class representing a synchronized pair of price-timestamp that can be immutably copied for snapshotting.

---
+++ src/Peak.java
@@ -0,0 +1,41 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * A price-timestamp pair to record a peak price trade event.
+ */
+public class Peak
+{
+    private double peakPricePerStock;
+    private long pricePeakTime;
+
+    public Peak(double peakPricePerStock, long pricePeakTime)
+    {
+        this.peakPricePerStock = peakPricePerStock;
+        this.pricePeakTime = pricePeakTime;
+    }
+
+    @JsonProperty
+    public synchronized Peak getImmutableCopy()
+    {
+        return new Peak(peakPricePerStock,pricePeakTime);
+    }
+
+
+    public synchronized void registerPeakHigh(double price, long time)
+    {
+        if(peakPricePerStock == Double.NaN || price > peakPricePerStock)
+        {
+            peakPricePerStock = price;
+            pricePeakTime = time;
+        }
+    }
+
+    public synchronized void registerPeakLow(double price, long time)
+    {
+        if(peakPricePerStock == Double.NaN || price < peakPricePerStock)
+        {
+            peakPricePerStock = price;
+            pricePeakTime = time;
+        }
+    }
+}
