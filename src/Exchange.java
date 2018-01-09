cf253e35405dc061efe9c27b9cf6680fd72adab7
Seriyin <redan.blue27@gmail.com>
2018-01-09 18:07:35 +0000

    Added the main exchange data class which contains the companies traded in it, as well as a contactable address if online.

---
+++ src/Exchange.java
@@ -0,0 +1,64 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+/**
+ * Exchange contains company information, it's name and whether
+ * it is online and on what address it is reachable.
+ */
+public class Exchange
+{
+    private boolean online;
+    private InetSocketAddress at;
+    private String name;
+    private Map<String,Company> companies;
+
+    @JsonProperty
+    public boolean isOnline()
+    {
+        return online;
+    }
+
+    @JsonProperty
+    public void setOnline(boolean online)
+    {
+        this.online = online;
+    }
+
+    @JsonProperty
+    public InetSocketAddress getAt()
+    {
+        return at;
+    }
+
+    @JsonProperty
+    public void setAt(InetSocketAddress at) {
+        this.at = at;
+    }
+
+    @JsonProperty
+    public String getName()
+    {
+        return name;
+    }
+
+    @JsonProperty
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @JsonProperty
+    public Map<String, Company> getCompanies()
+    {
+        return companies;
+    }
+
+    @JsonProperty
+    public void setCompanies(Map<String, Company> companies)
+    {
+        this.companies = companies;
+    }
+}
