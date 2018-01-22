7003a77de85069f3ba9195d76fb9fd8596fde139
Seriyin <redan.blue27@gmail.com>
2018-01-22 22:01:03 +0000

    Added a PUT method to update peak trading.

--- src/resources/DirectoryStats.java
+++ src/resources/DirectoryStats.java
@@ -2,16 +2,21 @@ package resources;
 
 import io.dropwizard.servlets.assets.ResourceNotFoundException;
 import model.Company;
+import model.Exchange;
 import model.Exchanges;
+import model.TradeCompleted;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 /**
  * Gets the live-stats for a company's trading in a given exchange.
  */
 @Path("/exchanges/{/name}{/company}")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class DirectoryStats
 {
     private Exchanges exchanges;
@@ -35,6 +40,40 @@ public class DirectoryStats
                                 .get(name)
                                 .getCompanies()
                                 .get(company);
+            }
+        }
+        throw new ResourceNotFoundException(new WebApplicationException());
+    }
+
+    @PUT
+    public Response updateCompanyTrades(@PathParam("name") String name,
+                                        @PathParam("company") String company,
+                                        TradeCompleted tc)
+    {
+        if(name.equals(tc.getExchange()) &&
+           company.equals(tc.getCompany()))
+        {
+            if (exchanges.getExchanges().containsKey(name))
+            {
+                Exchange e = exchanges.getExchanges()
+                                      .get(name);
+                if(e.isOnline()) {
+                    if(e.getCompanies()
+                        .containsKey(company))
+                    {
+                        Company c = exchanges.getExchanges()
+                                             .get(name)
+                                             .getCompanies()
+                                             .get(company);
+                        return Response.accepted(UriBuilder.fromResource(DirectoryStats.class)
+                                                           .build(name,company))
+                                       .build();
+                    }
+                }
+                else {
+                    throw new WebApplicationException("Exchange offline",
+                                                      Response.Status.FORBIDDEN);
+                }
             }
         }
         throw new ResourceNotFoundException(new WebApplicationException());
