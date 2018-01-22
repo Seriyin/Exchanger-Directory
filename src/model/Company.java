f71d772677bcb3ed9fcb6d2e750f8cd224d571f3
Seriyin <redan.blue27@gmail.com>
2018-01-22 20:57:14 +0000

    Added stat updating and checking whether exchange is live based on current time.

--- src/model/Company.java
+++ src/model/Company.java
@@ -1,6 +1,10 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.LocalDate;
 
 /**
  * A company need only have it's name, current and older statistical information ({@link Stats}) and whether it's currently live (means someone has traded on it for the current day).
@@ -8,9 +12,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Company
 {
     private String name;
-    private boolean live;
     private Stats yesterday;
     private Stats today;
+
+    @JsonCreator
+    public Company(@JsonProperty("name") String name,
+                   @JsonProperty("yesterday") Stats yesterday,
+                   @JsonProperty("today") Stats today)
+    {
+        this.name = name;
+        this.yesterday = yesterday;
+        this.today = today;
+    }
 
     @JsonProperty
     public String getName()
@@ -18,23 +31,15 @@ public class Company
         return name;
     }
 
-    @JsonProperty
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 
     @JsonProperty
     public boolean isLive()
     {
-        return live;
+        return new Interval(new DateTime(new java.util.Date()).withTime(9,0,0,0),
+                            new DateTime(new java.util.Date()).withTime(17,0,0,0))
+                       .containsNow();
     }
 
-    @JsonProperty
-    public void setLive(boolean live)
-    {
-        this.live = live;
-    }
 
     @JsonProperty
     public Stats getYesterday()
@@ -48,6 +53,7 @@ public class Company
         return today;
     }
 
+    @JsonProperty
     public Stats getStats(boolean current) {
         Stats result;
         if(current) {
@@ -57,6 +63,7 @@ public class Company
             return yesterday;
     }
 
+    @JsonProperty
     public CompanyDay createDay(boolean current)
     {
         if(current) {
@@ -64,6 +71,21 @@ public class Company
         }
         else{
             return new CompanyDay(name, getYesterday());
+        }
+    }
+
+    public void updateStats(TradeCompleted tc)
+    {
+        if(isLive()) {
+            if (today.getHighPeak()
+                     .getPeakPricePerStock() > tc.getTotal() / tc.getQuant())
+            {
+                today.registerPeakLow(tc.getTotal() / tc.getQuant(), tc.getTimestamp());
+            }
+            else
+            {
+                today.registerPeakHigh(tc.getTotal() / tc.getQuant(), tc.getTimestamp());
+            }
         }
     }
 }
